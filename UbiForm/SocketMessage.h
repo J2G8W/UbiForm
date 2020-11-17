646d01ba6a26b42ad940b8630418080d26fafc35
Julian W <julian@ashleywreford.com>
2020-11-17 10:14:39 +0000

    Add mutable of SocketMessage.h

--- UbiForm/SocketMessage.h
+++ UbiForm/SocketMessage.h
@@ -10,8 +10,14 @@ private:
     rapidjson::Document JSON_document;
 
 public:
-    SocketMessage(): JSON_document(){};
+    SocketMessage(): JSON_document(){
+        JSON_document.SetObject();
+    };
 
+    void addInteger(std::string &attributeName, int value){
+        rapidjson::Value key(attributeName.c_str(), JSON_document.GetAllocator());
+        JSON_document.AddMember(key, value, JSON_document.GetAllocator());
+    }
 
     char * stringify(){return stringifyDocument(JSON_document);};
 
