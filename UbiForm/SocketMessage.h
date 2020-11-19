65bc8b4356acacb862a32ab7f9be9fa76f9c96d8
Julian W <julian@ashleywreford.com>
2020-11-19 16:12:45 +0000

    Allow for overwriting of messages

--- UbiForm/SocketMessage.h
+++ UbiForm/SocketMessage.h
@@ -14,25 +14,41 @@ class SocketMessage {
 private:
     rapidjson::Document JSON_document;
 
+    // Takes in a key, value pair (or special type) and either replace the member, or add it
+    void addOrSwap(rapidjson::Value &key, rapidjson::Value &valueContainer){
+        if (JSON_document.HasMember(key)){
+            JSON_document[key] = valueContainer;
+        }else {
+            JSON_document.AddMember(key, valueContainer, JSON_document.GetAllocator());
+        }
+    }
+
 public:
     SocketMessage(): JSON_document(){
         JSON_document.SetObject();
     };
 
+    // Add a string value
     void addMember(const std::string &attributeName, const std::string &value){
         rapidjson::Value key(attributeName, JSON_document.GetAllocator());
         rapidjson::Value valueContainer(value, JSON_document.GetAllocator());
-        JSON_document.AddMember(key, valueContainer, JSON_document.GetAllocator());
+        addOrSwap(key,valueContainer);
     }
 
+    // Add an integer value
     void addMember(const std::string &attributeName, int value){
         rapidjson::Value key(attributeName, JSON_document.GetAllocator());
-        JSON_document.AddMember(key, value, JSON_document.GetAllocator());
+        rapidjson::Value valueContainer(value);
+        addOrSwap(key,valueContainer);
     }
+    // Add a boolean value
     void addMember(const std::string &attributeName, bool value){
         rapidjson::Value key(attributeName, JSON_document.GetAllocator());
-        JSON_document.AddMember(key, value, JSON_document.GetAllocator());
+        rapidjson::Value valueContainer(value);
+        addOrSwap(key,valueContainer);
     }
+
+
 
     char * stringify(){return stringifyDocument(JSON_document);};
 
--- UbiForm/test_SocketMessage.h
+++ UbiForm/test_SocketMessage.h
@@ -2,12 +2,28 @@
 
 #include "SocketMessage.h"
 
-TEST(SocketMessage, AddInteger){
+TEST(SocketMessage, AddMember){
     SocketMessage socketMessage;
     socketMessage.addMember( "A", 42);
     socketMessage.addMember("B" , true);
     socketMessage.addMember("C", std::string{"HELLO"});
 
     EXPECT_STREQ(socketMessage.stringify(), R"({"A":42,"B":true,"C":"HELLO"})");
+}
+
+TEST(SocketMessage, OverwriteInteger){
+    SocketMessage socketMessage;
+    socketMessage.addMember("A", 42);
+    socketMessage.addMember("A", 0);
+
+    EXPECT_STREQ(socketMessage.stringify(), R"({"A":0})");
+}
+
+TEST(SocketMessage, OverwriteString){
+    SocketMessage socketMessage;
+    socketMessage.addMember("A", std::string("HELLO"));
+    socketMessage.addMember("A", std::string("WORLD!"));
+
+    EXPECT_STREQ(socketMessage.stringify(), R"({"A":"WORLD!"})");
 }
 
