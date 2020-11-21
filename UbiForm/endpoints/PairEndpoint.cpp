e9b82949b848dfef7482bf5892faaad69ba3b08b
Julian W <julian@ashleywreford.com>
2020-11-21 14:27:19 +0000

    Implement PairEndpoint

---
+++ UbiForm/endpoints/PairEndpoint.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+#include <nng/nng.h>
+#include <nng/protocol/pair0/pair.h>
+
+
+#include "../general_functions.h"
+#include "PairEndpoint.h"
+
+
+
+
+// Note that outgoing means it dials an external URL
+void PairEndpoint::dialConnection(const char *url) {
+    int rv;
+    if ((rv = nng_pair0_open(senderSocket)) != 0) {
+        fatal("nng_pair0_open", rv);
+    }
+    // Use the same socket for sending and receiving
+    receiverSocket = senderSocket;
+    if ((rv = nng_dial(*senderSocket, url, nullptr, 0)) != 0) {
+        fatal("nng_dial", rv);
+    }
+
+}
+
+// Incoming means it will listen on an internal URL
+void PairEndpoint::listenForConnection(const char *url){
+    int rv;
+    if ((rv = nng_pair0_open(senderSocket)) != 0) {
+        fatal("nng_pair0_open", rv);
+    }
+    // Use the same socket for sending and receiving
+    receiverSocket = senderSocket;
+
+    if ((rv = nng_listen(*senderSocket, url, nullptr, 0)) != 0) {
+        fatal("nng_listen", rv);
+    }
+
+}
---
+++ UbiForm/endpoints/PairEndpoint.h
@@ -0,0 +1,28 @@
+#ifndef UBIFORM_PAIRENDPOINT_H
+#define UBIFORM_PAIRENDPOINT_H
+
+
+#include <unistd.h>
+#include "DataReceiverEndpoint.h"
+#include "DataSenderEndpoint.h"
+
+class PairEndpoint : DataReceiverEndpoint, DataSenderEndpoint {
+
+public:
+    void listenForConnection(const char *url) override ;
+    void dialConnection(const char *url) override;
+
+    // Destructor waits a short time before closing socket such that any unsent messages are released
+    ~PairEndpoint() {
+        int rv;
+        // Make sure that the messages are flushed
+        sleep(1);
+        // We only have one actual socket so only need to close 1
+        if ((rv = nng_close(*receiverSocket)) == NNG_ECLOSED) {
+            std::cerr << "This socket had already been closed" << std::endl;
+        }
+    }
+};
+
+
+#endif //UBIFORM_PAIRENDPOINT_H
