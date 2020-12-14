c94a0b012b85d8ae38e4fa26742703d7bf3daac4
Julian W <julian@ashleywreford.com>
2020-12-14 18:02:05 +0000

    Add a small test for the creation of components

--- UbiForm/test_Component.h
+++ UbiForm/test_Component.h
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
 
 #include "Component.h"
-class ComponentExample : public testing::Test{
+class SimpleComponent : public testing::Test{
 protected:
     // Note we aren't REALLY testing the inputting of the manifest as this is done automatically
-    ComponentExample(){
+    SimpleComponent(){
         component = new Component();
         if (pFile == NULL){
             std::cerr << "Error finding requisite file (JsonFiles/PairManifest1.json)";
@@ -12,7 +12,7 @@ protected:
         component->specifyManifest(pFile);
     }
 
-    ~ComponentExample(){
+    ~SimpleComponent(){
         delete component;
     }
 
@@ -21,7 +21,7 @@ protected:
 };
 
 
-TEST_F(ComponentExample, CreateMultipleEndpoints){
+TEST_F(SimpleComponent, CreateMultipleEndpoints){
     component->createNewPublisherEndpoint("v1", "PubEndpoint");
     component->createNewSubscriberEndpoint("v1", "SubEndpoint");
 
@@ -29,7 +29,7 @@ TEST_F(ComponentExample, CreateMultipleEndpoints){
     ASSERT_NO_THROW(component->getSenderEndpointById("PubEndpoint"));
 }
 
-TEST_F(ComponentExample, GetEndpoints){
+TEST_F(SimpleComponent, GetEndpoints){
     component->createNewPairEndpoint("v1","TestEndpoint");
 
     ASSERT_NO_THROW(component->getReceiverEndpointById("TestEndpoint"));
@@ -39,6 +39,43 @@ TEST_F(ComponentExample, GetEndpoints){
     ASSERT_ANY_THROW(component->getReceiverEndpointById("NotAnEndpoint"));
 }
 
-TEST_F(ComponentExample, WrongEndpointType){
+TEST_F(SimpleComponent, WrongEndpointType){
     ASSERT_ANY_THROW(component->createNewPairEndpoint("NotAType","TestEndpoint"));
+}
+
+class PairBasedComponent : public testing::Test{
+protected:
+    // Note we aren't REALLY testing the inputting of the manifest as this is done automatically
+    PairBasedComponent(){
+        receiverComponent = new Component();
+        senderComponent = new Component();
+        if (pFile == NULL){
+            std::cerr << "Error finding requisite file (JsonFiles/PairManifest1.json)";
+        }
+        receiverComponent->specifyManifest(pFile);
+        fseek(pFile,0, SEEK_SET);
+        senderComponent->specifyManifest(pFile);
+
+    }
+
+    ~PairBasedComponent(){
+        delete receiverComponent;
+        delete senderComponent;
+    }
+
+    FILE* pFile = fopen("JsonFiles/PairManifest1.json", "r");
+    Component* receiverComponent;
+    Component* senderComponent;
+};
+
+TEST_F(PairBasedComponent, FindEachOther){
+    const char *address = "ipc:///tmp/test.ipc";
+    // We use IPC to test our component
+    senderComponent->startBackgroundListen(address);
+    ASSERT_NO_THROW(receiverComponent->requestPairConnection(address, "v1"));
+
+    sleep(1);
+    ASSERT_NO_THROW(receiverComponent->getReceiverEndpointsByType("v1")->at(0));
+    ASSERT_NO_THROW(senderComponent->getSenderEndpointsByType("v1")->at(0));
+
 }
