75803aed5eb338c6a9ac879a780c7d3a93f40e8b
Julian W <julian@ashleywreford.com>
2020-12-17 17:30:06 +0000

    More complex Resource Discovery example has two publishers availble, want to find both

--- examples/ResourceDiscoveryExample.cpp
+++ examples/ResourceDiscoveryExample.cpp
@@ -5,13 +5,19 @@
 #include <chrono>
 #include <iomanip>
 
+// Hub is a Publisher and an RDH
 #define HUB "HUB"
+// Connection is a subscriber
 #define CONNECTION "CONNECTION"
+// Publisher is JUST a publisher (different to Hub)
+#define PUBLISHER "PUBLISHER"
+
+#define RDHlocation "tcp://127.0.0.1:7999"
 
 int main(int argc, char ** argv){
     if (argc >= 2){
         if (strcmp(argv[1], HUB) == 0){
-            Component component;
+            Component component("tcp://127.0.0.1");
 
             FILE* pFile = fopen("JsonFiles/PublisherManifest1.json", "r");
             if (pFile == nullptr) perror("ERROR");
@@ -20,15 +26,15 @@ int main(int argc, char ** argv){
 
             std::cout << "MANIFEST SPECIFIED" << std::endl;
 
-            component.startBackgroundListen("tcp://127.0.0.1:8000");
+            component.startBackgroundListen(8000);
             std::cout << "Component Listening" << std::endl;
 
-            component.startResourceDiscoveryHub("tcp://127.0.0.1:7999");
+            component.startResourceDiscoveryHub(7999);
 
             std::cout << "Resource discovery started" << std::endl;
 
             ResourceDiscoveryConnEndpoint* rdc = component.createResourceDiscoveryConnectionEndpoint();
-            rdc->registerWithHub("tcp://127.0.0.1:7999");
+            rdc->registerWithHub(RDHlocation);
 
             std::cout << "Registered successfully" << std::endl;
 
@@ -51,11 +57,47 @@ int main(int argc, char ** argv){
                 }
                 sleep(1);
             }
+        }if (strcmp(argv[1], PUBLISHER) == 0){
+            Component component("tcp://127.0.0.3");
 
+            FILE* pFile = fopen("JsonFiles/PublisherManifest1.json", "r");
+            if (pFile == nullptr) perror("ERROR");
+            component.specifyManifest(pFile);
+            fclose(pFile);
+
+            std::cout << "MANIFEST SPECIFIED" << std::endl;
+
+            component.startBackgroundListen(8000);
+            std::cout << "Component Listening" << std::endl;
+
+            ResourceDiscoveryConnEndpoint* rdc = component.createResourceDiscoveryConnectionEndpoint();
+            rdc->registerWithHub(RDHlocation);
+
+            std::cout << "Registered successfully" << std::endl;
+
+
+            SocketMessage s;
+            bool valid = true;
+            auto publisherEndpoints = component.getSenderEndpointsByType("publisherExample");
+            while(true) {
+                if (!publisherEndpoints->empty()) {
+                    s.addMember("reverse", valid);
+                    valid = !valid;
+
+                    char dateString[30];
+                    time_t t = time(nullptr);
+                    struct tm *p = localtime(&t);
+                    strftime(dateString, 30, "%A %B", p);
+                    s.addMember("date", std::string(dateString));
+
+                    publisherEndpoints->at(0)->sendMessage(s);
+                }
+                sleep(1);
+            }
         }
         if (strcmp(argv[1], CONNECTION) == 0){
-            Component component;
-            component.startBackgroundListen("tcp://127.0.0.2:8000");
+            Component component("tcp://127.0.0.2");
+            component.startBackgroundListen(8000);
 
             FILE* pFile = fopen("JsonFiles/SubscriberManifest1.json", "r");
             if (pFile == nullptr) perror("ERROR");
@@ -64,27 +106,20 @@ int main(int argc, char ** argv){
 
             std::cout << "MANIFEST SPECIFIED" << "\n";
 
+
+            const char * locationOfRDH = RDHlocation;
+
             ResourceDiscoveryConnEndpoint* rdc = component.createResourceDiscoveryConnectionEndpoint();
+            rdc->addResourceDiscoveryHub(locationOfRDH);
 
-            rdc->addResourceDiscoveryHub("tcp://127.0.0.1:7999");
-
-            rdc->registerWithHub("tcp://127.0.0.1:7999");
-            std::vector<std::string> ids = rdc->getComponentIdsFromHub("tcp://127.0.0.1:7999");
+            rdc->registerWithHub(locationOfRDH);
+            std::vector<std::string> ids = rdc->getComponentIdsFromHub(locationOfRDH);
 
             std::cout << "Available ids: ";
             for (const auto& i: ids){ std::cout << i << ' ';}
             std::cout << std::endl;
 
-            std::vector<SocketMessage *> newConnection = rdc->getComponentsBySchema("subscriberExample");
-            std::cout << "Connectables: ";
-            for (const auto& i: newConnection){
-                std::cout << i->stringify() << std::endl;
-                component.requestAndCreateConnection("subscriberExample",
-                                                     i->getString("url"),
-                                                     i->getString("endpointType"));
-            }
-            std::cout << std::endl;
-
+            rdc->createEndpointBySchema("subscriberExample");
 
             std::unique_ptr<SocketMessage> s;
             auto subscriberEndpoints = component.getReceiverEndpointsByType("subscriberExample");
@@ -101,7 +136,6 @@ int main(int argc, char ** argv){
                     }
                 }
             }
-
         }
     }
     else{
