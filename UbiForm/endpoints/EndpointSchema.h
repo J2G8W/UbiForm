33556681c198dfc87102d2c6fad4065a114e4915
Julian W <julian@ashleywreford.com>
2020-12-17 12:10:06 +0000

    Ability to build schema from FILE

--- UbiForm/endpoints/EndpointSchema.h
+++ UbiForm/endpoints/EndpointSchema.h
@@ -1,8 +1,11 @@
 #ifndef UBIFORM_ENDPOINTSCHEMA_H
 #define UBIFORM_ENDPOINTSCHEMA_H
 
-
+#include <rapidjson/document.h>
 #include <rapidjson/schema.h>
+#include <rapidjson/filereadstream.h>
+#include <rapidjson/writer.h>
+
 #include "../SocketMessage.h"
 
 class EndpointSchema {
@@ -11,6 +14,18 @@ private:
 
 public:
     explicit EndpointSchema(rapidjson::Value &doc) : schema(doc) { }
+    explicit EndpointSchema(FILE *jsonFP) : schema(InitiateFromFile(jsonFP)){
+    }
+
+    // TODO - make this less nasty
+    static rapidjson::Document InitiateFromFile(FILE *jsonFP){
+        rapidjson::Document JSON_document;
+        char readBuffer[65536];
+        rapidjson::FileReadStream inputStream(jsonFP, readBuffer, sizeof(readBuffer));
+        JSON_document.ParseStream(inputStream);
+        return JSON_document;
+    }
+
     void validate(const SocketMessage &messageToValidate);
 };
 
