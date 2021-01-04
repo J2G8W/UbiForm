c8cb554b7e620c5e573ff9888517a2306cacd6b7
Julian W <julian@ashleywreford.com>
2021-01-04 13:39:47 +0000

    Simplify SystemSchemas tests

--- UbiForm/SystemSchemas/test_SystemSchemas.h
+++ UbiForm/SystemSchemas/test_SystemSchemas.h
@@ -7,18 +7,9 @@ TEST(SystemSchemas, Init){
 
 TEST(SystemSchemas, ComponentSchema){
     SystemSchemas ss;
-    FILE* pFile = fopen("TestManifests/Component1.json", "r");
-    if (pFile == nullptr){
-        std::cout << "Error opening file - " << std::endl;
-        throw std::system_error();
-    }
 
-    rapidjson::Document JSON_document;
-    char readBuffer[65536];
-    rapidjson::FileReadStream inputStream(pFile, readBuffer, sizeof(readBuffer));
-    JSON_document.ParseStream(inputStream);
 
-    fclose(pFile);
+    rapidjson::Document * JSON_document = parseFromFile("TestManifests/Component1.json");
 
-    ASSERT_NO_THROW(ss.getSystemSchema(SystemSchemaName::componentManifest).validate(JSON_document));
+    ASSERT_NO_THROW(ss.getSystemSchema(SystemSchemaName::componentManifest).validate(*JSON_document));
 }
