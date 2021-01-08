02efc4ccdb06f686cfcd3058227de396558176cf
Julian W <julian@ashleywreford.com>
2021-01-08 11:13:56 +0000

    Keep making Integration test larger

--- tests/ReconfigurationIntegrationTest.h
+++ tests/ReconfigurationIntegrationTest.h
@@ -1,26 +1,22 @@
-class ReconfigurationIntegrationTest : public testing::Test{
-protected:
+TEST(ReconfigurationIntegrationTest, IntegrationTest1){
+    Component receiverComponent("ipc:///tmp/comp1");
+    Component senderComponent("ipc:///tmp/comp2");
 
-    ReconfigurationIntegrationTest(): receiverComponent("ipc:///tmp/comp1"), senderComponent("ipc:///tmp/comp2"){
-        receiverComponent.specifyManifest(R"({"name":"TEST1","schemas":{}})");
-        senderComponent.specifyManifest(R"({"name":"TEST2","schemas":{}})");
-    }
 
-    ~ReconfigurationIntegrationTest() override = default;
+    receiverComponent.specifyManifest(R"({"name":"TEST1","schemas":{}})");
+    senderComponent.specifyManifest(R"({"name":"TEST2","schemas":{}})");
 
-    Component receiverComponent;
-    Component senderComponent;
-};
-
-TEST_F(ReconfigurationIntegrationTest, IntegrationTest1){
     receiverComponent.startBackgroundListen();
     senderComponent.startBackgroundListen();
 
     auto newEs = std::make_shared<EndpointSchema>();
     newEs->addProperty("msg", ValueType::String);
     newEs->addRequired("msg");
-    senderComponent.getBackgroundRequester().requestAddEndpoint(receiverComponent.getBackgroundListenAddress(),"generatedSubscriber",
-                                                                nullptr, newEs.get(),SocketType::Subscriber);
+    senderComponent.getBackgroundRequester().requestChangeEndpoint(receiverComponent.getBackgroundListenAddress(),
+                                                                   SocketType::Subscriber,
+                                                                   "generatedSubscriber",
+                                                                   newEs.get(),
+                                                                   nullptr);
     senderComponent.getComponentManifest()->addEndpoint(SocketType::Publisher,"generatedPublisher",
                                                         nullptr, newEs);
     nng_msleep(300);
@@ -45,4 +41,62 @@ TEST_F(ReconfigurationIntegrationTest, IntegrationTest1){
     auto receiveOriginal = receiverEndpoints->at(0)->receiveMessage();
 
     ASSERT_EQ(original.getString("msg"),receiveOriginal->getString("msg"));
+}
+
+
+TEST(ReconfigurationIntegrationTest, IntegrationTest2){
+    Component RDH("ipc:///tmp/RDH");
+    Component receiverComponent("ipc:///tmp/comp1");
+    Component senderComponent("ipc:///tmp/comp2");
+
+    RDH.specifyManifest(R"({"name":"HUB","schemas":{}})");
+    receiverComponent.specifyManifest(R"({"name":"RECEIVER","schemas":{}})");
+    senderComponent.specifyManifest(R"({"name":"SENDER","schemas":{}})");
+
+    RDH.startResourceDiscoveryHub(7999);
+    std::string rdhLocation = "ipc:///tmp/RDH:7999";
+
+    RDH.startBackgroundListen();
+    receiverComponent.startBackgroundListen();
+    senderComponent.startBackgroundListen();
+
+    RDH.getResourceDiscoveryConnectionEndpoint()->registerWithHub(rdhLocation);
+    receiverComponent.getResourceDiscoveryConnectionEndpoint()->registerWithHub(rdhLocation);
+    senderComponent.getResourceDiscoveryConnectionEndpoint()->registerWithHub(rdhLocation);
+
+
+    ASSERT_EQ(RDH.getResourceDiscoveryConnectionEndpoint()->getResourceDiscoveryHubs().size(),1);
+    ASSERT_EQ(receiverComponent.getResourceDiscoveryConnectionEndpoint()->getResourceDiscoveryHubs().size(),1);
+    ASSERT_EQ(senderComponent.getResourceDiscoveryConnectionEndpoint()->getResourceDiscoveryHubs().size(),1);
+
+    std::string receiverComponentId = receiverComponent.getResourceDiscoveryConnectionEndpoint()->getId(
+            receiverComponent.getResourceDiscoveryConnectionEndpoint()->getResourceDiscoveryHubs().at(0));
+
+    auto receiverRep = receiverComponent.getResourceDiscoveryConnectionEndpoint()->getComponentById(rdhLocation, receiverComponentId);
+
+    ASSERT_NO_THROW(receiverRep->getName());
+    ASSERT_EQ(receiverRep->getName(), "RECEIVER");
+
+    auto newEndpointSchema = std::make_shared<EndpointSchema>();
+    newEndpointSchema->addProperty("value", ValueType::Number);
+    newEndpointSchema->addRequired("value");
+    receiverComponent.getComponentManifest()->addEndpoint(SocketType::Subscriber, "genSubscriber",
+                                                          newEndpointSchema, nullptr);
+    receiverComponent.updateManifestAtResourceDiscoveryHubs();
+
+    receiverRep = receiverComponent.getResourceDiscoveryConnectionEndpoint()->getComponentById(rdhLocation, receiverComponentId);
+
+    ASSERT_NO_THROW(receiverRep->getReceiverSchema("genSubscriber"));
+    ASSERT_EQ(newEndpointSchema->stringify(), receiverRep->getReceiverSchema("genSubscriber")->stringify());
+
+    receiverComponent.getBackgroundRequester().requestChangeEndpoint(senderComponent.getBackgroundListenAddress(),
+                                                                     SocketType::Publisher,
+                                                                     "genPublisher",
+                                                                     nullptr, newEndpointSchema.get());
+    // Takes time for the background senderComponent to register with RDH
+    nng_msleep(300);
+
+    auto locations = receiverComponent.getResourceDiscoveryConnectionEndpoint()->getComponentsBySchema("genSubscriber");
+    ASSERT_EQ(locations.size(),1);
+    ASSERT_EQ(locations.at(0)->getString("url"), senderComponent.getBackgroundListenAddress());
 }
