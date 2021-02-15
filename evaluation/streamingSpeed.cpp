119f40795d30c9ddd9ab05e55773cb4a1cc088ef
Julian W <julian@ashleywreford.com>
2021-02-15 15:53:16 +0000

    Add block size to measurements

--- evaluation/streamingSpeed.cpp
+++ evaluation/streamingSpeed.cpp
@@ -12,13 +12,15 @@ struct timingData{
     std::chrono::duration<int64_t, std::nano> endTime;
     std::chrono::duration<int64_t, std::nano> duration;
     long fileSize;
-    std::ostringstream outputStream;
+    int blockSize;
+    std::ostringstream* outputStream;
 };
 
 void endOfReceiveStream(PairEndpoint* pe, void* userData){
     auto* t = static_cast<timingData*>(userData);
     t->endTime = std::chrono::high_resolution_clock::now().time_since_epoch();
-    t->fileSize = t->outputStream.tellp();
+    t->fileSize = t->outputStream->tellp();
+    delete t->outputStream;
 }
 
 struct startupData{
@@ -26,35 +28,43 @@ struct startupData{
     Component * component;
     std::string fileName;
     bool streamDone = false;
+    int blockSize = 10002;
 };
 void endOfSenderStream(PairEndpoint* pe, void* userData){
     auto* t = static_cast<startupData*>(userData);
+    t->fileStream.close();
     t->streamDone = true;
 }
 void senderConnectStream(Endpoint* e, void* userData){
     auto* t = static_cast<startupData*>(userData);
     SocketMessage sm;
+    sm.addMember("blockSize", t->blockSize);
     auto pair = t->component->castToPair(e);
 
     t->fileStream.open(t->fileName, std::fstream::binary | std::fstream::in);
     if(!t->fileStream.good()) {
         std::cerr << "Unable to open file " << t->fileName << std::endl;
-        exit(1);
     }
 
-    pair->sendStream(t->fileStream, 10002, false, sm, endOfSenderStream, t);
+    pair->sendStream(t->fileStream, t->blockSize, false, sm, endOfSenderStream, t);
 }
 
 
 
 int main(int argc, char **argv) {
-    if (argc >= 2) {
-        if (strcmp(argv[1], RECEIVER) == 0 && argc>=3) {
+    if (argc >= 3) {
+        if (strcmp(argv[1], RECEIVER) == 0) {
             Component receiver;
             std::shared_ptr<EndpointSchema> empty = std::make_shared<EndpointSchema>();
-            receiver.getComponentManifest().addEndpoint(SocketType::Pair,"receiver",empty,empty);
+            std::shared_ptr<EndpointSchema> receiveEndpoint = std::make_shared<EndpointSchema>();
+            receiveEndpoint->addProperty("blockSize",ValueType::Number);
+            receiveEndpoint->addRequired("blockSize");
+            receiver.getComponentManifest().addEndpoint(SocketType::Pair,"receiver",receiveEndpoint,empty);
 
             timingData ts[5];
+            for(auto &t:ts){
+                t.outputStream = new std::ostringstream ;
+            }
 
             for(auto & t : ts) {
                 t.startTime = std::chrono::high_resolution_clock::now().time_since_epoch();
@@ -71,7 +81,8 @@ int main(int argc, char **argv) {
                 std::shared_ptr<PairEndpoint> pair = receiver.castToPair(endpoints->at(0));
 
 
-                pair->receiveStream(t.outputStream, endOfReceiveStream, &t);
+                auto rcv = pair->receiveStream(*(t.outputStream), endOfReceiveStream, &t);
+                t.blockSize = rcv->getInteger("blockSize");
 
                 while (!pair->getReceiverThreadEnded()) {
                     nng_msleep(100);
@@ -82,18 +93,29 @@ int main(int argc, char **argv) {
             results.open("results.txt",std::fstream::out | std::fstream::app);
             for(auto &t : ts){
                 t.duration = t.endTime - t.startTime;
-                results << t.duration.count() << "," << t.fileSize << "\n";
+                results << t.duration.count() << "," << t.fileSize << "," << t.blockSize << "\n";
             }
             results.close();
         } else if (strcmp(argv[1], SENDER) == 0) {
             Component sender;
 
             std::shared_ptr<EndpointSchema> empty = std::make_shared<EndpointSchema>();
-            sender.getComponentManifest().addEndpoint(SocketType::Pair,"sender",empty,empty);
+            std::shared_ptr<EndpointSchema> senderSchema = std::make_shared<EndpointSchema>();
+            senderSchema->addProperty("blockSize",ValueType::Number);
+            senderSchema->addRequired("blockSize");
+            sender.getComponentManifest().addEndpoint(SocketType::Pair,"sender",empty,senderSchema);
 
             auto* s = new startupData;
             s->fileName = std::string(argv[2]);
             s->component = &sender;
+            if(argc >=4 ){
+                try {
+                    s->blockSize = std::stoi(argv[3]);
+                }catch (std::logic_error &e){
+                    std::cerr << e.what() << std::endl;
+                    exit(-1);
+                }
+            }
             sender.registerStartupFunction("sender",senderConnectStream,s);
 
             sender.startBackgroundListen(8000);
@@ -104,8 +126,6 @@ int main(int argc, char **argv) {
                 }
                 s->streamDone = false;
                 sender.closeAndInvalidateSocketsOfType("sender");
-                s->fileStream.close();
-                //std::cout << i << std::endl;
             }
         } else {
             std::cerr << "Error usage is " << argv[0] << " " << RECEIVER << " SENDER_ADDRESS\n";
