0b002de27b20f0ca08b08780e4c41731052688f1
Julian W <julian@ashleywreford.com>
2021-02-19 09:16:23 +0000

    Shortened delay for Publisher startup

--- evaluation/pubSubSpeed.cpp
+++ evaluation/pubSubSpeed.cpp
@@ -52,7 +52,7 @@ struct publisherStartupData{
     int messagesSent;
 };
 void publisherStartup(Endpoint * e, void * d){
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     auto* userData = static_cast<publisherStartupData*>(d);
     DataSenderEndpoint* publisher = userData->component->castToDataSenderEndpoint(e);
     SocketMessage sm;
