96465f27832af0211c8836afb582251d2cb56a49
Julian W <julian@ashleywreford.com>
2021-02-19 18:11:26 +0000

    Made MusicPlayerExample better

--- examples/MusicPlayerExample.cpp
+++ examples/MusicPlayerExample.cpp
@@ -1,11 +1,8 @@
-
 #include <cstring>
-
 #include "../include/UbiForm/Component.h"
 #include <nng/supplemental/util/platform.h>
 #include <fstream>
 #include <SFML/Audio.hpp>
-#include <SFML/System.hpp>
 #include <SFML/System/FileInputStream.hpp>
 
 
@@ -13,109 +10,127 @@
 #define SENDER "SENDER"
 
 
-int main(int argc, char **argv) {
-    if (argc >= 3) {
-        if (strcmp(argv[1], RECEIVER) == 0 ) {
-            Component receiver;
-            std::shared_ptr<EndpointSchema> recv = std::make_shared<EndpointSchema>();
-            recv->addProperty("extraInfo",ValueType::String);
-            recv->addRequired("extraInfo");
-            std::shared_ptr<EndpointSchema> empty = std::make_shared<EndpointSchema>();
-            receiver.getComponentManifest().addEndpoint(SocketType::Pair,"receiver",recv,empty);
 
-            std::cout << "MANIFEST SPECIFIED" << "\n";
+struct PairStartupInfo{
+    Component* component;
+    sf::Music musicObject;
+    sf::FileInputStream stream;
+    bool progEnd = false;
+    bool musicPlaying = false;
+    PairStartupInfo() : musicObject(), stream(){}
+};
 
-            receiver.getBackgroundRequester().requestRemoteListenThenDial(
-                    argv[2], 8000, "receiver",
-                    "sender");
-            auto endpoints = receiver.getEndpointsByType("receiver");
-            if(endpoints->empty()){
-                throw std::logic_error("Oops couldn't create connection");
-            }
+void onPairStreamEnd(PairEndpoint *pEndpoint, void *pVoid){
+    auto* musicFileStream = static_cast<std::fstream*>(pVoid);
+    musicFileStream->close();
+    delete musicFileStream;
+}
 
-            std::fstream musicFileStream;
-            musicFileStream.open("temp.wav", std::fstream::binary | std::fstream::out);
-            musicFileStream.flush();
+void onStreamingEndpointCreation(Endpoint* e, void* u){
+    auto* userData = static_cast<PairStartupInfo*>(u);
 
-            std::shared_ptr<PairEndpoint> pair = receiver.castToPair(endpoints->at(0));
+    if (userData->musicPlaying){
+        userData->musicObject.stop();
+        userData->musicPlaying = false;
+    }
+
+    std::string tempFileName;
+    tempFileName = "temp" + std::to_string(rand() % 10000) + ".wav";
+    auto* musicFileStream = new std::fstream;
+    musicFileStream->open(tempFileName, std::fstream::binary | std::fstream::out);
+    musicFileStream->flush();
+
+    auto* pair = userData->component->castToPair(e);
+    auto extraInfo = pair->receiveStream(*musicFileStream, onPairStreamEnd, musicFileStream);
+    std::cout << "Extra info: " << extraInfo->getString("extraInfo") << std::endl;
+
+    nng_msleep(100);
+    musicFileStream->flush();
+    std::cout << "INITIATING PLAY" << std::endl;
 
 
-            auto extraInfo = pair->receiveStream(musicFileStream, nullptr, nullptr);
-            std::cout << "Extra info: " << extraInfo->getString("extraInfo") << std::endl;
+    if (!userData->stream.open(tempFileName)){
+        throw std::logic_error("Error with file");
+    }
+    if(!userData->musicObject.openFromStream(userData->stream)){
+        throw std::logic_error("Error with stream");
+    }
+    userData->musicObject.play();
+    userData->musicPlaying = true;
+    std::cout << "PLAYING" << std::endl;
+}
 
-            /*
-            while (!pair->getReceiverThreadEnded()) {
-                nng_msleep(1000);
-            }
-            musicFileStream.close();
-             */
-            musicFileStream.flush();
-            std::cout << "INITIATING PLAY" << std::endl;
-            sf::Music musicObject;
-            sf::FileInputStream stream;
-            if (!stream.open("temp.wav")){
-                throw std::logic_error("Error with file");
-            }
-            if(!musicObject.openFromStream(stream)){
-                throw std::logic_error("Error with stream");
-            }
-            musicObject.play();
-            std::cout << "PLAYING" << std::endl;
-            auto recentPos = musicObject.getPlayingOffset();
-            while(true){
-                if (musicObject.getStatus() == musicObject.Stopped){
-                    if (stream.tell() == stream.getSize()){
-                        break;
-                    }
-                    musicObject.play();
-                    musicObject.setPlayingOffset(recentPos);
-                    std::cout << "RESET" << std::endl;
-                }else {
-                    recentPos = musicObject.getPlayingOffset();
+
+
+void maintainMusicPlaying(PairStartupInfo* psi){
+    auto recentPos = psi->musicObject.getPlayingOffset();
+    while(true){
+        if(psi->progEnd){
+            break;
+        }else if (psi->musicPlaying) {
+            if (psi->musicObject.getStatus() == psi->musicObject.Stopped) {
+                if (psi->stream.tell() == psi->stream.getSize()) {
+                    psi->musicPlaying = false;
+                    continue;
                 }
-                nng_msleep(10);
+                psi->musicObject.play();
+                psi->musicObject.setPlayingOffset(recentPos);
+                std::cout << "RESET" << std::endl;
+            } else {
+                recentPos = psi->musicObject.getPlayingOffset();
             }
-            std::cout << "End of music reached" << std::endl;
-            musicFileStream.close();
-
-        } else if (strcmp(argv[1], SENDER) == 0) {
-            Component sender;
-
-            std::shared_ptr<EndpointSchema> send = std::make_shared<EndpointSchema>();
-            send->addProperty("extraInfo",ValueType::String);
-            send->addRequired("extraInfo");
-            std::shared_ptr<EndpointSchema> empty = std::make_shared<EndpointSchema>();
-            sender.getComponentManifest().addEndpoint(SocketType::Pair,"sender",empty,send);
-
-            std::cout << "MANIFEST SPECIFIED" << "\n";
-
-            sender.startBackgroundListen(8000);
-            auto endpointVector = sender.getEndpointsByType("sender");
-            while (endpointVector->empty()){
-                nng_msleep(100);
-            }
-            auto pair = sender.castToPair(endpointVector->at(0));
-            std::ifstream file;
-
-            SocketMessage initalMsg;
-            initalMsg.addMember("extraInfo","Stream incoming");
-
-            file.open(argv[2], std::fstream::binary | std::fstream::in);
-            if(!file.good()){
-                std::cerr << "Unable to open file " << argv[2] << std::endl;
-                exit(1);
-            }
-            pair->sendStream(file, 10002, false, initalMsg, nullptr, nullptr);
-
-            while (!pair->getSenderThreadEnded()) {
-                nng_msleep(1000);
-            }
-            file.close();
-
-        } else {
-            std::cerr << "Error usage is " << argv[0] << " " << RECEIVER << "SENDER_ADDRESS\n";
-            std::cerr << argv[0] << " " << SENDER << " [fileLocation]" << std::endl;
         }
+        nng_msleep(10);
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc >= 1) {
+        Component receiver;
+        std::shared_ptr<EndpointSchema> recv = std::make_shared<EndpointSchema>();
+        recv->addProperty("extraInfo",ValueType::String);
+        recv->addRequired("extraInfo");
+        std::shared_ptr<EndpointSchema> empty = std::make_shared<EndpointSchema>();
+        receiver.getComponentManifest().addEndpoint(SocketType::Pair,"receiver",recv,empty);
+
+        auto* pairStartupInfo = new PairStartupInfo;
+        pairStartupInfo->component = &receiver;
+        receiver.registerStartupFunction("receiver", onStreamingEndpointCreation, pairStartupInfo);
+
+
+        std::thread maintainMusic(maintainMusicPlaying, pairStartupInfo);
+        while(true) {
+            std::string userInput;
+            std::cin >> userInput;
+            if (userInput == "end") {
+                pairStartupInfo->progEnd = true;
+                break;
+            } else if (userInput.rfind("add_rdh", 0) == 0) {
+                std::cout << "Option for Resource Discovery Hub (\"search\" or [uri]): ";
+                std::string option;
+                std::cin >> option;
+                if (option == "search") {
+                    std::cout << "Searching for Resource Discovery Hubs" << std::endl;
+                    receiver.getResourceDiscoveryConnectionEndpoint().searchForResourceDiscoveryHubs();
+                } else {
+                    try {
+                        receiver.getResourceDiscoveryConnectionEndpoint().registerWithHub(option);
+                    } catch (std::logic_error &e) {
+                        std::cerr << "Unable to add Resource Discovery Hub at " << option << std::endl;
+                    }
+                }
+            } else if (userInput.rfind("connect", 0) == 0) {
+                std::cout << "Dial address: ";
+                std::string option;
+                std::cin >> option;
+                receiver.getBackgroundRequester().requestRemoteListenThenDial(
+                        option, 8000, "receiver",
+                        "sender");
+            }
+        }
+        std::cout << "End of music reached" << std::endl;
+        maintainMusic.join();
+        delete pairStartupInfo;
     } else {
         std::cerr << "Error usage is " << argv[0] << " " << RECEIVER << "SENDER_ADDRESS\n";
         std::cerr << argv[0] << " " << SENDER << " [fileLocation]" << std::endl;
