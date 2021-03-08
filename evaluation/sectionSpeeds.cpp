9ffa2fe2c22f63e67a96339291be98fc4e024fb7
Julian W <julian@ashleywreford.com>
2021-03-08 15:11:01 +0000

    Started sectionSpeeds evaluation

---
+++ evaluation/sectionSpeeds.cpp
@@ -0,0 +1,103 @@
+#include <cstring>
+
+#include "../include/UbiForm/Component.h"
+#include <nng/supplemental/util/platform.h>
+
+#define RECEIVER "RECEIVER"
+#define SENDER "SENDER"
+
+#define USE_RDH false
+
+
+struct ReceiverData{
+    Component & component;
+    std::vector<std::chrono::duration<int64_t, std::nano>>& timings;
+    bool end = false;
+    ReceiverData(Component& c, std::vector<std::chrono::duration<int64_t, std::nano>>& t) : component(c), timings(t) {}
+};
+void receiverCreationCallback(Endpoint* e, void* data){
+    auto userData = static_cast<ReceiverData *>(data);
+    userData->timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+
+    auto message = userData->component.castToPair(e)->receiveMessage();
+    userData->timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+    userData->end = true;
+}
+
+struct SenderData{
+    Component & component;
+    std::vector<std::chrono::duration<int64_t, std::nano>>& timings;
+    SenderData(Component& c, std::vector<std::chrono::duration<int64_t, std::nano>>& t) : component(c), timings(t) {}
+};
+void senderCreationCallback(Endpoint* e, void* data){
+    auto userData = static_cast<SenderData *>(data);
+    userData->timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+
+    SocketMessage sm;
+    sm.addMember("stringProperty", "Hello");
+    sm.addMember("numberProperty", 42);
+    std::unique_ptr<SocketMessage> subObject = std::make_unique<SocketMessage>();
+    subObject->addMember("subString","world!");
+    sm.addMoveObject("objectProperty", std::move(subObject));
+    userData->timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+
+    userData->component.castToPair(e)->sendMessage(sm);
+    userData->timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+}
+
+
+int main(int argc, char **argv) {
+    if (argc >= 2) {
+        if (strcmp(argv[1], RECEIVER) == 0) {
+            Component receiver;
+            FILE *pFile = fopen("EvaluationFiles/EvaluationManifest1.json", "r");
+            if (pFile == nullptr) perror("ERROR");
+            receiver.specifyManifest(pFile);
+            fclose(pFile);
+
+            receiver.startBackgroundListen();
+
+            std::vector<std::chrono::duration<int64_t, std::nano>> timings;
+            auto* data = new ReceiverData(receiver, timings);
+            receiver.registerStartupFunction("pairEvaluation", receiverCreationCallback,data);
+
+            while (!data->end) {
+                nng_msleep(100);
+            }
+            for (auto x : timings){
+                std::cout << x.count() << std::endl;
+            }
+        } else if (strcmp(argv[1], SENDER) == 0 && argc >=3) {
+            std::vector<std::chrono::duration<int64_t, std::nano>> timings;
+            timings.reserve(5);
+
+            // Initial time
+            timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+            Component sender;
+            FILE *pFile = fopen("EvaluationFiles/EvaluationManifest1.json", "r");
+            if (pFile == nullptr) throw std::logic_error("Could not find JsonFiles/PairManifest1.json");
+            sender.specifyManifest(pFile);
+            fclose(pFile);
+            auto* data = new SenderData(sender, timings);
+            sender.registerStartupFunction("pairEvaluation",senderCreationCallback, data);
+
+            // Time after starting component
+            timings.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
+            sender.startBackgroundListen();
+            sender.getBackgroundRequester().localListenThenRequestRemoteDial(
+                    argv[2], "pairEvaluation","pairEvaluation");
+
+
+            nng_msleep(3000);
+            for(auto x : timings){
+                std::cout << x.count() << std::endl;
+            }
+
+
+        } else {
+            std::cerr << "Error usage is " << argv[0] << " " << RECEIVER << "|" << SENDER << "\n";
+        }
+    } else {
+        std::cerr << "Error usage is " << argv[0] << " " << RECEIVER << "|" << SENDER << "\n";
+    }
+}
