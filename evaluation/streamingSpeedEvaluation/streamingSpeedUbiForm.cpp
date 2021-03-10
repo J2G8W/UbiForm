02d1c3e637da016fc8f3db700fca7ce2b9317d2c
Julian W <julian@ashleywreford.com>
2021-03-10 15:52:56 +0000

    Undo changes to streamingSpeed test

--- evaluation/streamingSpeedEvaluation/streamingSpeedUbiForm.cpp
+++ evaluation/streamingSpeedEvaluation/streamingSpeedUbiForm.cpp
@@ -7,11 +7,6 @@
 #define RECEIVER "RECEIVER"
 #define SENDER "SENDER"
 
-// https://stackoverflow.com/questions/11826554/standard-no-op-output-stream
-class NullBuffer : public std::streambuf{
-public:
-    int overflow(int c) { return c; }
-};
 
 struct timingData{
     std::chrono::duration<int64_t, std::nano> startTime;
@@ -19,7 +14,7 @@ struct timingData{
     std::chrono::duration<int64_t, std::nano> duration;
     long fileSize;
     int blockSize;
-    std::ostream* outputStream;
+    std::ostringstream* outputStream;
 };
 
 void endOfReceiveStream(PairEndpoint* pe, void* userData){
@@ -67,10 +62,8 @@ int main(int argc, char **argv) {
             receiver.getComponentManifest().addEndpoint(SocketType::Pair,"receiver",receiveEndpoint,empty);
 
             timingData ts[5];
-
             for(auto &t:ts){
-                auto* null_buffer = new NullBuffer;
-                t.outputStream = new std::ostream(null_buffer) ;
+                t.outputStream = new std::ostringstream ;
             }
 
             for(auto & t : ts) {
