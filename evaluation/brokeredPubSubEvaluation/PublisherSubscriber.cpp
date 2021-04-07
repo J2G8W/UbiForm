c9a8e27713cf281cbfbb126e8f9af0eb34ea4abe
Julian <julian@ashleywreford.com>
2021-04-07 12:12:36 +0100

    Small changes to Brokered Pub/Sub

--- evaluation/brokeredPubSubEvaluation/PublisherSubscriber.cpp
+++ evaluation/brokeredPubSubEvaluation/PublisherSubscriber.cpp
@@ -48,6 +48,23 @@ void subscriberStartup(Endpoint* e, void*d){
     userData->done = true;
 }
 
+std::string random_string( size_t length )
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+                "0123456789"
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                "abcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
+}
+
+
 int main(int argc, char **argv) {
     if (strcmp(argv[1], SUBSCRIBER_COMPONENT) == 0 && argc >= 3) {
         Component component;
@@ -55,6 +72,8 @@ int main(int argc, char **argv) {
         std::shared_ptr <EndpointSchema> es = std::make_shared<EndpointSchema>();
         es->addProperty("counter", ValueType::Number);
         es->addRequired("counter");
+        es->addProperty("extraData",ValueType::String);
+        es->addRequired("extraData");
         component.getComponentManifest().addEndpoint(ConnectionParadigm::Subscriber, "speedSub", es, nullptr);
 
         auto *userData = new subscriberStartupData;
@@ -87,6 +106,8 @@ int main(int argc, char **argv) {
         std::shared_ptr <EndpointSchema> sendSchema = std::make_shared<EndpointSchema>();
         sendSchema->addProperty("counter", ValueType::Number);
         sendSchema->addRequired("counter");
+        sendSchema->addProperty("extraData",ValueType::String);
+        sendSchema->addRequired("extraData");
 
         std::shared_ptr <EndpointSchema> receiveSchema = std::make_shared<EndpointSchema>();
         receiveSchema->addProperty("success", ValueType::Boolean);
@@ -95,6 +116,13 @@ int main(int argc, char **argv) {
                                                      receiveSchema, sendSchema);
 
         component.startBackgroundListen();
+
+
+        std::vector<std::string> randomStrings(MESSAGE_NUM);
+        for(int i = 0; i < MESSAGE_NUM; i++){
+            randomStrings[i] = random_string(300);
+        }
+
 
         std::vector<std::chrono::duration<int64_t, std::nano>> messageSendTimes;
         std::vector<std::chrono::duration<int64_t, std::nano>> messageReplyTimes;
@@ -123,9 +151,10 @@ int main(int argc, char **argv) {
         EndpointMessage sm;
         int i = 0;
         for (; i < MESSAGE_NUM; i++) {
+            messageSendTimes.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
             sm.addMember("counter", i);
+            sm.addMember("extraData",randomStrings[i]);
             try {
-                messageSendTimes.push_back(std::chrono::high_resolution_clock::now().time_since_epoch());
                 sendEndpoint->sendMessage(sm);
                 auto msg = recvEndpoint->receiveMessage();
                 if (!msg->getBoolean("success")) {
