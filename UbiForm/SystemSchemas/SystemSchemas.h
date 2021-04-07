8ebb8311912910b299b799caee1a1ebb356df156
Julian <julian@ashleywreford.com>
2021-04-07 12:55:10 +0100

    SystemSchemas location made more flexible via CMake

--- UbiForm/SystemSchemas/SystemSchemas.h
+++ UbiForm/SystemSchemas/SystemSchemas.h
@@ -18,27 +18,29 @@ public:
      * @throws AccessError when we can't find a file
      */
     SystemSchemas() {
-        const char *files[16] = {"SystemSchemas/component_schema.json",
-                                 "SystemSchemas/endpoint_creation_request.json",
-                                 "SystemSchemas/endpoint_creation_response.json",
-                                 "SystemSchemas/resource_discovery_addition_request.json",
-                                 "SystemSchemas/resource_discovery_addition_response.json",
-                                 "SystemSchemas/resource_discovery_by_id_request.json",
-                                 "SystemSchemas/resource_discovery_by_id_response.json",
-                                 "SystemSchemas/resource_discovery_by_schema_request.json",
-                                 "SystemSchemas/resource_discovery_by_schema_response.json",
-                                 "SystemSchemas/resource_discovery_component_ids_request.json",
-                                 "SystemSchemas/resource_discovery_component_ids_response.json",
-                                 "SystemSchemas/resource_discovery_update_request.json",
-                                 "SystemSchemas/general_resource_discovery_request.json",
-                                 "SystemSchemas/general_resource_discovery_response.json",
-                                 "SystemSchemas/general_endpoint_request.json",
-                                 "SystemSchemas/general_endpoint_response.json"};
+        const char *files[16] = {"/component_schema.json",
+                                 "/endpoint_creation_request.json",
+                                 "/endpoint_creation_response.json",
+                                 "/resource_discovery_addition_request.json",
+                                 "/resource_discovery_addition_response.json",
+                                 "/resource_discovery_by_id_request.json",
+                                 "/resource_discovery_by_id_response.json",
+                                 "/resource_discovery_by_schema_request.json",
+                                 "/resource_discovery_by_schema_response.json",
+                                 "/resource_discovery_component_ids_request.json",
+                                 "/resource_discovery_component_ids_response.json",
+                                 "/resource_discovery_update_request.json",
+                                 "/general_resource_discovery_request.json",
+                                 "/general_resource_discovery_response.json",
+                                 "/general_endpoint_request.json",
+                                 "/general_endpoint_response.json"};
 
         for (int i = 0; i < 16; i++) {
-            FILE *pFile = fopen(files[i], "r");
+            std::string file = SYSTEM_SCHEMAS_LOC + std::string(files[i]);
+            FILE *pFile = fopen(file.c_str(), "r");
             if (pFile == nullptr) {
                 std::string errorMsg = "Error opening file - " + std::string(files[i]);
+                std::cerr << errorMsg << std::endl;
                 auto es = std::make_shared<GenericSchema>();
                 systemSchemas.insert(std::make_pair(static_cast<SystemSchemaName>(i), es));
             } else {
--- UbiForm/SystemSchemas/test_SystemSchemas.h
+++ UbiForm/SystemSchemas/test_SystemSchemas.h
@@ -12,5 +12,7 @@ TEST(SystemSchemas, ComponentSchema) {
     rapidjson::Document *JSON_document = parseFromFile("TestManifests/Component1.json");
 
     ASSERT_NO_THROW(ss.getSystemSchema(SystemSchemaName::componentManifest).validate(*JSON_document));
+    // Make sure we haven't got blank schemas coming in
+    ASSERT_THROW(ss.getSystemSchema(SystemSchemaName::additionRequest).validate(*JSON_document), ValidationError);
     delete JSON_document;
 }
